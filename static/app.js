1b41ccbd1b00f4ed3862fc7e949009b075377020
KhiemBui9198 <chikhiemkm9198@gmail.com>
2023-03-31 11:37:54 +0700

    update animation

--- static/app.js
+++ static/app.js
@@ -5,20 +5,18 @@ class Chatbox {
             chatBox: document.querySelector('.chatbox__support'),
             sendButton: document.querySelector('.send__button')
         }
-
         this.state = false;
         this.messages = [];
     }
 
     display() {
-        const {openButton, chatBox, sendButton} = this.args;
-
+        this.hideElem();
+        const { openButton, chatBox, sendButton } = this.args;
         openButton.addEventListener('click', () => this.toggleState(chatBox))
 
         sendButton.addEventListener('click', () => this.onSendButton(chatBox))
-
         const node = chatBox.querySelector('input');
-        node.addEventListener("keyup", ({key}) => {
+        node.addEventListener("keyup", ({ key }) => {
             if (key === "Enter") {
                 this.onSendButton(chatBox)
             }
@@ -27,9 +25,8 @@ class Chatbox {
 
     toggleState(chatbox) {
         this.state = !this.state;
-
         // show or hides the box
-        if(this.state) {
+        if (this.state) {
             chatbox.classList.add('chatbox--active')
             if (this.isfirst == null) {
                 let msg2 = { name: "Sam", message: 'Xin chào tôi là chat bot tư vấn, tôi có thể giúp gì cho bạn ?' };
@@ -41,57 +38,61 @@ class Chatbox {
             chatbox.classList.remove('chatbox--active')
         }
     }
-
+    hideElem() {
+        document.getElementById("myTyping").style.visibility = "hidden";
+      }
+      
+    showElem() {
+        document.getElementById("myTyping").style.visibility = "visible";
+      }
     onSendButton(chatbox) {
+        this.showElem();
         var textField = chatbox.querySelector('input');
         let text1 = textField.value
         if (text1 === "") {
             return;
-        }
-        textField.value = '';
+        }textField.value = ' ';
         let msg1 = { name: "User", message: text1 }
         this.messages.push(msg1);
-
-        fetch('https://chatbotopenai.azurewebsites.net/predict', {
+        fetch('/predict', {
             method: 'POST',
             body: JSON.stringify({ message: text1 }),
             mode: 'cors',
             headers: {
-              'Content-Type': 'application/json'
+                'Content-Type': 'application/json'
             },
-          })
-          .then(r => r.json())
-          .then(r => {
-            let msg2 = { name: "Sam", message: r.answer };
-            this.messages.push(msg2);
-            this.updateChatText(chatbox)
-            textField.value = ''
+        })
+            .then(r => r.json())
+            .then(r => {
+                let msg2 = { name: "Sam", message: r.answer };
+                this.messages.push(msg2);
+                this.updateChatText(chatbox)
+                textField.value = '';
+                this.hideElem();
 
-        }).catch((error) => {
-            console.error('Error:', error);
-            this.updateChatText(chatbox)
-            textField.value = ''
-          });
+            }).catch((error) => {
+                console.error('Error:', error);
+                this.updateChatText(chatbox)
+                textField.value = ''
+            });
     }
 
     updateChatText(chatbox) {
         var html = '';
-        this.messages.slice().reverse().forEach(function(item, index) {
-            if (item.name === "Sam")
-            {
+        this.messages.slice().reverse().forEach(function (item, index) {
+            if (item.name === "Sam") {
                 html += '<div class="messages__item messages__item--visitor">' + item.message + '</div>'
             }
-            else
-            {
+            else {
                 html += '<div class="messages__item messages__item--operator">' + item.message + '</div>'
             }
-          });
-
+        });
         const chatmessage = chatbox.querySelector('.chatbox__messages');
         chatmessage.innerHTML = html;
     }
+
 }
-
-
 const chatbox = new Chatbox();
 chatbox.display();
+
+
